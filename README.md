1af2c328026f3b3bcf14b5d76349970ee25fdd8a
golia <giovvani.terzuolo01@gmail.com>
2022-04-22 18:24:50 +0200

    Added documentation for glob and refactoring of README

--- README.md
+++ README.md
@@ -1,61 +1,24 @@
 # GT_SO_PROJECT_21-22
 
 ----
+# Description of the project
 
-Note for the programmer:
-in any case if termination of the node proc a message would be pushed into master messagge queue
-if available before termination occure
+
 # Requirements
 
-* Unix Sys.
-* GCC
+* Doxygen: for rendering the documentation
+* Unix Sys: Tested on Ubuntu 21.10
+* GCC or GDB
 * Cmake
 
 # Installing
 
-*
+    # Using .zip file
 
-# Principal Process Running
-* Printing:
-    if we want to save current budget into bookmaster shm the problem is that i'm saving to much info
-        into the shm consuming bytes. so it'would be great to get info via message report queue
-        by sending snapshot of users
+    # Using git
 
-# Node Proc
-Node_tp_shm_key => current_node.pid
-node_tp_semaphore => current_node.id
+# Processes types running
 
-L'apt della reward per singola transazione viene fatta solo quando block size e caricato -> parte più critica
-    *SHM: part of the responsability of a node proc is to create the tp_shm that will be used by the
-         node for loading the transaction_block to be processed
-    *Node_tp_proc: it just start one tp_proc and kill it when node_proc terminate. (Maybe can be
-     implemented a waiting time out of wich node_proc terminate in case of no new block loaded into
-     tp shm)
-Note for the programmer:
-in any case if termination of the node proc a message would be pushed into master messagge queue
-if available before termination occure
-# Node Tp Proc
-*  Scopes:
-    1)  Keep the queue empty from all the messages related to is node parent. Serve and mantain updated
-        the tp_shm memory in order to allow node_proc to process all the blocks in order
-    2)  Make sure that the tp_size is respected and advice user in case of tp_size full
-    3)  In case of executing the 30 point version it is able to handle the transaction incoming when
-        the tp size is full. If full it will send the transaction to another node
-# User Proc
 
-Viene tenuto in vita fino a che la lista di transazioni non è vuota (if cashing is on). otherwise check for balance and
-terminate if balance is > 2 o riceve il segnale di terminazione o il bilancio non lo permette.
-
-ACTUALLY THERE ARE DIFFERENT CHOICES FOR ENDING USER PROC:
-
-1) MAKE IT RUN FOREVER UNTIL ALARM SEND FROM MAIN -> TIMEOUT
-2) MAKE IT PAUSE ON BALANCE <2 AND RESUME IT
-3) MAKE IT END ON BALANCE <2 AND RUN A NEW PROC WITH SAME CHARACTERISTICS IF MONEY ARRIVES
-4) MAKE IT END WITHOUT CONSIDERING INCOMING FLOW OF CASH
-
-- further implementation would be to resume the user (because can listen to the same ref into the message queue)
-# Transazioni
-    -> timestamp della transazione è caricato una prima volta dallo user per poi essere aggiornato dal
-    nodo
-    -> quando lo user la crea il reward non è ancora impostato sulla transazione
+# Information about the chosen implementation
 
--- src/local_lib/headers/glob.h
+++ src/local_lib/headers/glob.h
@@ -28,24 +28,60 @@
 /*! \def SEMAPHORE_MASTER_BOOK_ACCESS_KEY
  * \brief The key for the masterbook semaphore for single block access
  */
-#define SEMAPHORE_MASTER_BOOK_ACCESS_KEY 0x61 /*For single block access*/
+#define SEMAPHORE_MASTER_BOOK_ACCESS_KEY 0x61
 /*! \def SEMAPHORE_MASTER_BOOK_TO_FILL_KEY
  * \brief The key for the masterbook semaphore for to_fill shm param
  */
-#define SEMAPHORE_MASTER_BOOK_TO_FILL_KEY 0X62 /*For to_fill shm param look for def into shm_masterbook*/
+#define SEMAPHORE_MASTER_BOOK_TO_FILL_KEY 0X62
 /*  QUEUE KEY  values from 71 to 80*/
+
+/*! \def MASTER_QUEUE_KEY
+ * \brief The key for the master report queue
+ */
 #define MASTER_QUEUE_KEY 0x71
+/*! \def USERS_QUEUE_KEY
+ * \brief The key for the user report queue
+ */
 #define USERS_QUEUE_KEY 0x72
+/*! \def NODE_QUEUE_KEY
+ * \brief The key for the node report queue
+ */
 #define NODES_QUEUE_KEY 0x73
+
 /*  SHARED MEM. KEY values from 81 to 90*/
+/*! \def SHM_CONFIGURATION
+ * \brief The key for the shm dedicated to the configuration
+ */
 #define SHM_CONFIGURATION 0x81
+/*! \def SHM_MASTER_BOOK
+ * \brief The key for the shm dedicated to the master book
+ */
 #define MASTER_BOOK_SHM_KEY 0x82
+
 /*  COLOR DEFINITION    */
+/*! \def COLOR_RED_ANSI_CODE
+ * \brief The color red in ansi code
+ */
 #define COLOR_RED_ANSI_CODE "\x1b[31m"
+/*! \def COLOR_GREEN_ANSI_CODE
+ * \brief The color green in ansi code
+ */
 #define COLOR_GREEN_ANSI_CODE "\x1b[32m"
+/*! \def COLOR_YELLOW_ANSI_CODE
+ * \brief The color yellow in ansi code
+ */
 #define COLOR_YELLOW_ANSI_CODE "\x1b[33m"
+/*! \def COLOR_BLUE_ANSI_CODE
+ * \brief The color blue in ansi code
+ */
 #define COLOR_BLUE_ANSI_CODE    "\x1b[34m"
+/*! \def COLOR_MAGENTA_ANSI_CODE
+ * \brief The color magenta in ansi code
+ */
 #define COLOR_WHITE_ANSI_CODE "\x1b[37m"
+/*! \def COLOR_RESET_ANSI_CODE
+ * \brief The color reset in ansi code
+ */
 #define COLOR_RESET_ANSI_CODE "\x1b[0m"
 /*AT COMPILATION TIME DEFINITION*/
 #ifndef SO_REGISTRY_SIZE
@@ -55,19 +91,22 @@
 #define SO_BLOCK_SIZE 10
 #endif
 /*  GENERAL UTILITY MACROS*/
-#define REALLOC_MARGIN 10 /*delta in wich is not usefull to realloc TODO: make a function that calculate it*/
-#define MAX_FAILURE_SHM_LOADING 1000
-#define MAX_UNSED_CICLE_OF_NODE_PROC 10000000
-#define MAX_WAITING_TIME_FOR_UPDATE 10000
-#define MAX_FAILURE_SHM_BOOKMASTER_LOCKING 10
+#define REALLOC_MARGIN 10 /**< delta in wich is not usefull to realloc TODO: make a function that calculate it*/
+#define MAX_FAILURE_SHM_LOADING 1000 /**< max number of times that the shm loading can fail*/
+#define MAX_UNSED_CICLE_OF_NODE_PROC 10000000 /**< max number of times that the node process can be unused*/
+#define MAX_WAITING_TIME_FOR_UPDATE 10000 /**< max number of times for updating a specific sys call*/
+#define MAX_FAILURE_SHM_BOOKMASTER_LOCKING 10 /**< max number of times that the shm locking can fail*/
 
 /*SIMULATION END TYPES*/
-enum simulation_end_type {
-    SIMULATION_END_BY_USER,
-    SIMULATION_END_BY_TIME,
-    SIMULATION_END_BY_SO_REGISTRY_FULL,
-    SIMULATION_END_BY_NO_PROC_RUNNING,
-    SIMULATION_END_PROPERLY_TERMINATED,
+/*! \enum SIMULATION_END_TYPE
+ * \brief The type of the simulation that can occured
+ */
+enum SIMULATION_END_TYPE{
+    SIMULATION_END_BY_USER, /**< The simulation is ended by the user*/
+    SIMULATION_END_BY_TIME, /**< The simulation is ended by the time*/
+    SIMULATION_END_BY_SO_REGISTRY_FULL, /**< The simulation is ended by the registry full*/
+    SIMULATION_END_BY_NO_PROC_RUNNING, /**< The simulation is ended by the no proc running*/
+    SIMULATION_END_PROPERLY_TERMINATED, /**< The simulation is ended by the properly terminated*/
 };
 
 #endif /*DGT_SO_PROJECT_21_22_GLOB_H*/
