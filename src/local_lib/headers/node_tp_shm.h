1fdc07fc1a22cc613ea698802447de403d8aa6f6
golia <giovvani.terzuolo01@gmail.com>
2022-04-22 18:31:41 +0200

    Documentation upgrade

--- src/local_lib/headers/node_tp_shm.h
+++ src/local_lib/headers/node_tp_shm.h
@@ -1,17 +1,22 @@
 #ifndef DGT_SO_PROJECT_21_22_NODE_TP_SHM_H
 #define DGT_SO_PROJECT_21_22_NODE_TP_SHM_H
+
 #include "transaction_list.h"
 #include "glob.h"
-/* Can be implemented in one variable using bitwise shifting, for reading purpose it's implemented
- * in the following way
- * */
+
+/*! \def IS_EMPTY
+ * \brief Empty shm cell
+ */
 #define IS_EMPTY 1
+/*! \def IS_FULL
+ * \brief Full shm cell
+ */
 #define FULL 2
-/**
+/*! \struct node_block
  * To load this struct it's raccomanded to use a semaphore with a semval set to FULL from the writer
  * and reset to IS_EMPTY from the reader, in order to make data consistent.
  */
 struct node_block{
-    struct Transaction block_t[SO_BLOCK_SIZE];
+    struct Transaction block_t[SO_BLOCK_SIZE]; /**!< Array of transactions */
 };
 #endif/*DGT_SO_PROJECT_21_22_NODE_TP_SHM_H*/
